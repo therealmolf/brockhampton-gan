232d0715bf961b1df6580c90128c1c63c5257d2d
Miko Planas <miksbon@gmail.com>
2021-03-25 13:22:29 +0800

    image gen works but slow

--- inference/views.py
+++ inference/views.py
@@ -10,6 +10,7 @@ import base64
 from inference.helpers import display_image, to_data_uri, flattened_pca
 from inference.deep import create_model
 import re
+import time
 
 
 progan = hub.load("https://tfhub.dev/google/progan-128/1").signatures['default']
@@ -41,22 +42,50 @@ def generate_image(request):
     file_obj = request.FILES['filepath']
     print(file_obj)
     fs = FileSystemStorage()
+    
+    t0 = time.perf_counter()
+    ####################################
     filepath_name = fs.save(file_obj.name,file_obj)
     filepath_name = fs.url(filepath_name)
+
     # hacky format thing I had to do
     filepath_name = re.sub("%20", " ", filepath_name)
     filepath_name = "/home/therealmolf/projects/gango" + filepath_name
+    ####################################
+    t1 = time.perf_counter()
+    print(f"File saving took {t1-t0} seconds ")
 
+
+    ####################################
     # filepath_name = "/home/therealmolf/projects/gango/media/glaive - dnd (audio).mp3"
-
-
     model = create_model()
-    
+    ####################################
+    t2 = time.perf_counter()
+    print(f"Model creation took {t2-t1} seconds")
+
+
+    ####################################
     vector = flattened_pca(filepath_name)
+    ####################################
+    t3 = time.perf_counter()
+    print(f"flatten pca function took {t3-t2} seconds")
+    print(vector.shape)
+
+    vector = tf.random.normal([1, 100_000])
+
+    # BOTTLENECK IS HERE
+    ####################################
     output = model(vector)[0]
+    ####################################
+    t4 = time.perf_counter()
+    print(f"model output took {t4-t3} seconds")
+
+    ####################################
     pil_img = display_image(output)
     music_uri = to_data_uri(pil_img)
-
+    ####################################
+    t5 = time.perf_counter()
+    print(f"displaying the image toook {t5-t4} seconds")
     # print(filepath_name)
 
     context = {'filepath_name': filepath_name,
